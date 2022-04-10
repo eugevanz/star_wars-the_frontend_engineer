9f366342613c7c7d769c56d46cb2c02c023fc9e4
Eugene Van Zyl <eugevanz@gmail.com>
2022-04-10 11:55:23 +0200

    First manual commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Link from "next/link";
 
-const Thing: React.FC<{ msg: string }> = props => {
+const Thing: React.FC<{ msg: string }> = (props) => {
   return <div>{props.msg}</div>;
 };
 
 export default () => (
   <div>
     Hello World. <Thing msg="hello" />
-    <Link href="/about">
-      <a href='/about'>About</a>
-    </Link>
+    <Link href="/about">About</Link>
   </div>
 );
